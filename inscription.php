8452b6f2a126602889b5d45378f14dee1a3ce140
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2021-12-30 15:09:01 +0100

    BDD créée sur PHPmyADMIN

---
+++ inscription.php
@@ -0,0 +1,40 @@
+<?php
+
+
+?>
+
+
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Inscription</title>
+    
+    </head>
+
+    <body>
+
+     <!-- REQUIRE LE HEADER -->
+
+        <main>
+
+            <form action="inscription.php" method="post">
+                
+                <input type="text" name="login" placeholder="login">
+                <input type="text" name="prenom" placeholder="prenom">
+                <input type="text" name="email" placeholder="email">
+                <input type="text" name="confemail" placeholder="Confirmer l'email">
+                <input type="password" name="password" placeholder="Mot de passe">
+                <input type="password" name="confpassword" placeholder="Confirmer le mot de passe">
+                <input type="submit" name="submit">
+
+
+
+
+
+
+     <!-- REQUIRE LE FOOTER -->
+    
+   
+    </body>
+</html>
