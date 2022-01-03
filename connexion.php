a704e8319482ae86334ab11e50a027aef055ce07
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2022-01-03 22:43:12 +0100

    CONNEXION => CREATION STRUCTURE HTML

---
+++ connexion.php
@@ -0,0 +1,27 @@
+<?php
+
+?>
+
+
+<html lang="fr">
+    <head>
+        <meta charset="utf-8">
+        <title>Inscription</title> 
+    </head>
+
+    <body>
+
+      <?php require('header.php')?>
+
+        <main>
+            <div class="2blocs">
+                <div class="signinbox">
+                    
+                </div>
+            </div>
+        </main>
+
+     <!-- REQUIRE LE FOOTER -->
+    
+    </body>
+</html>
