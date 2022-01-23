3c18eb5a9d5358a151dcca06e95296aa4b6c4f53
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2022-01-23 23:03:08 +0100

    PLANNING => en cours affichage des réservation
    La fonctiond 'affichage des réservation à la date correspondante est fonctionnelle. Reste à choisir quelles infos doivent être affichées, (nom, prenom, numéro de résa)

--- model/test.php
+++ model/test.php
@@ -4,12 +4,7 @@ require ('classes/class_reservations.php');
 require ('classes/class_user.php');
 require ('bdd.php');
 
-$date = new Reservations();
-$date->BetweenDates();
-
-
-
-
-
+$betweener = new Reservations;
+var_dump($betweener->BetweenDates());
 
 ?>
