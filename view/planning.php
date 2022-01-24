5db5b750b322b919e94b6ade65172f82d93181a5
thomas serdjebi <thomas.serdjebi@laplateforme.io>
2022-01-24 14:23:16 +0100

    PLANNING  / ADMIN RESERVATIO?NS
    ajustement de l'affichage du planning, si un lieu n'est pas choisi dans l'URL alors il ne s'affiche pas.
    Page de listing des reservations en cours pour 'ladmin avec l'affichage sur le côté  de toutes les infos de la réservation

--- view/planning.php
+++ view/planning.php
@@ -23,6 +23,7 @@ $currentyear = date('Y');
 
 
 
+
 ?>
 
 <html lang="fr">
@@ -43,78 +44,85 @@ $currentyear = date('Y');
             <h1>Planning mensuel</h1>
 
             
-            <div class="d-flex flex-row align-itmes-center justify-content-around">
+                
+                <div class="d-flex flex-row align-itmes-center justify-content-around">
 
-                <a href="planning.php?location=plage&month=<?php echo $currentmonth; ?>&year=<?php echo $currentyear;?>">La Plage</a>
-                <a href="planning.php?location=pins&month=<?php echo $currentmonth ;?>&year=<?php echo $currentyear;?>">Les Pins</a>
-                <a href="planning.php?location=maquis&month=<?php echo $currentmonth; ?>&year=<?php echo $currentyear;?>">Le Maquis</a>
+                    <a href="planning.php?location=plage&month=<?php echo $currentmonth; ?>&year=<?php echo $currentyear;?>">La Plage</a>
+                    <a href="planning.php?location=pins&month=<?php echo $currentmonth ;?>&year=<?php echo $currentyear;?>">Les Pins</a>
+                    <a href="planning.php?location=maquis&month=<?php echo $currentmonth; ?>&year=<?php echo $currentyear;?>">Le Maquis</a>
 
-            </div>
-
-
-            <div class="d-flex flex-row align-itmes-center justify-content-between">
-
-                <h2><?php echo $month->toString() ; ?></h2>
-
-                <div>
-                    <a href="planning.php?location=<?php echo @$_GET['location']?>&month=<?php echo $month->previousMonth()->month?>&year=<?php echo $month->previousMonth()->year?>" class="btn btn-primary">&lt;</a>
-                    <a href="planning.php?location=<?php echo @$_GET['location']?>&month=<?php echo $month->nextMonth()->month?>&year=<?php echo $month->nextMonth()->year?>" class="btn btn-primary">&gt;</a>
                 </div>
 
-            </div>
+             <?php if (@$_GET['location']== null) { 
+                echo "<div> Choisissez un lieu pour afficher son planning </div>" ; ?>
 
-            <table class="planning__table planning__table--<?php echo $month->getWeeks(); ?>weeks">
-
-                <?php for ($i=0; $i < $month->getWeeks(); $i++) {?>
-
-                    <tr>
-
-                        <?php foreach($month->days as $k => $day) {
-
-                           $date = (clone $start)->modify("+" .($k + $i * 7). "days")?>
-
-                            
-                            <td class="<?php if($month->withinMonth($date)===false) {echo "planning__othermonth";}?>">
-
-                                <?php if ($i === 0 ) { ?> <div class="planning__weekday"><?php echo $day ; } ?></div>
-
-                                <div class="planning__day">
-                            
-                                    <?php echo $date->format('d') ;?>
-                
-                                </div>
-
-                                <div> 
-                                    <?php 
-                                        $newdate = new Reservations;
-                                        $newdate->DisplayReservation($date);
-                                          
-                                    ?>
-                                </div>
-
-                                <div>
-                                    <?php
-                                
-
-                                    ?>
+            <?php } else {?>
 
 
-                                </div>
+                <div class="d-flex flex-row align-itmes-center justify-content-between">
+
+                    <h2><?php echo $month->toString() ; ?></h2>
+
+                    <div>
+                        <a href="planning.php?location=<?php echo @$_GET['location']?>&month=<?php echo $month->previousMonth()->month?>&year=<?php echo $month->previousMonth()->year?>" class="btn btn-primary">&lt;</a>
+                        <a href="planning.php?location=<?php echo @$_GET['location']?>&month=<?php echo $month->nextMonth()->month?>&year=<?php echo $month->nextMonth()->year?>" class="btn btn-primary">&gt;</a>
+                    </div>
+
+                </div>
+
+                <table class="planning__table planning__table--<?php echo $month->getWeeks(); ?>weeks">
+
+                    <?php for ($i=0; $i < $month->getWeeks(); $i++) {?>
+
+                        <tr>
+
+                            <?php foreach($month->days as $k => $day) {
+
+                            $date = (clone $start)->modify("+" .($k + $i * 7). "days")?>
 
                                 
+                                <td class="<?php if($month->withinMonth($date)===false) {echo "planning__othermonth";}?>">
+
+                                    <?php if ($i === 0 ) { ?> <div class="planning__weekday"><?php echo $day ; } ?></div>
+
+                                    <div class="planning__day">
+                                
+                                        <?php echo $date->format('d') ;?>
+                    
+                                    </div>
+
+                                    <div> 
+                                        <?php 
+                                            $newdate = new Reservations;
+                                            $newdate->DisplayReservation($date);
+                                            
+                                        ?>
+                                    </div>
+
+                                    <div>
+                                        <?php
+                                    
+
+                                        ?>
 
 
-                            </td>
+                                    </div>
 
-                        <?php } ?>
-                    </tr>
-
+                                    
 
 
-                <?php }?>
+                                </td>
+
+                            <?php } ?>
+                        </tr>
 
 
-            </table>
+
+                    <?php }?>
+
+
+                </table>
+            <?php }?>
 
             
 
--- view/reservations.php
+++ view/reservations.php
@@ -1,3 +1,12 @@
 <?php
 
+session_start();
+
+require('../model/classes/class_reservations.php');
+require('../model/classes/class_user.php');
+require('../model/classes/class_admin.php');
+
+$check_admin = new Admin;
+$check_admin->CheckAdmin();
+
 ?>
