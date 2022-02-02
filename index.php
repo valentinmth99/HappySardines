5559bef2c7e1706ec2ed6af2a6cb60b9cb94b09e
Valentin Mathieu <valentin.mathieu@laplateforme.io>
2022-02-02 16:28:44 +0100

    CORRECTION BUGS CSS

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ session_start();
         <title>Accueil</title> 
         <link rel="stylesheet" href="view/style/header.css">
         <link rel="stylesheet" href="view/style/index.css">
+        <link rel="stylesheet" href="view/style/footer.css">
     </head>
 
     <body>
@@ -49,26 +50,53 @@ session_start();
 
             <section class="container">
 
-                <h1>Camping <br>
-
-                    Happy Sardines
-                </h1>
+                <h1>Camping <br> Happy Sardines</h1>
                 
-                <p>
-
-                    Installez vous chez Happy Sardines</br>
-                    
+                <p> Installez vous chez Happy Sardines</br>
                     et découvrez le coeur de la FRENCH RIVIERA.</br>
-
                     Découvrez le site <a href="view/lecamping.php">ici</a>.
-
                 </p>
-            </section>
 
+            </section>
 
         </main>
 
-        <?php require('view/footer.php'); ?>
+        <footer>
+
+            <div class="footerboxes">
+
+                <div class="footerleftboxes">
+
+                    <div> Ce site Web a été conçu par : </div>
+                    <div> Thomas <b>Serdjebi</b> </div>
+                    <div> Valentin <b>Mathieu</b> </div>
+                    <div> Etudiants à l'école du numérique La Plateforme_ </div>
+                    <div> Retrouvez le projet sur : <a class="lienfooter" href="https://github.com/thomas-serdjebi" target="blank">le GitHub de Thomas</a> et <a class="lienfooter" href="https://github.com/valentin-mathieu" target="blank">le Github de Valentin<a/>.</div>
+
+                </div>
+
+                <div class="footerrightboxes">
+
+                    <div id="rdvfoot"> Retrouvez toutes les infos sur <a href="https://laplateforme.io/" target="_blank" class="lienfooter">La Plateforme_</a></div>
+
+                    <div><a href="https://laplateforme.io/" class="lienfooter"><img src="view/assets/logo_lp.png" alt="logoplateforme" class="logoplateforme"></div>
+
+                        <div class="footersocialboxes">
+
+                            <div><a href="https://www.facebook.com/LaPlateformeIO" target="_blank"><img src="view/assets/logo_fb.JPG" alt="logofacebook"  class="logosocial"></a></div>
+
+                            <div><a href="https://www.linkedin.com/school/laplateformeio/" target="_blank"><img src="view/assets/logo_linkedin.JPG" alt="logolinkedin" class="logosocial"></a></div>
+
+                            <div><a href="https://twitter.com/LaPlateformeIO" target="_blank"><img src="view/assets/logo_twitter.JPG" alt="logotwitter" class="logosocial"></a></div>
+
+                            <div><a href="https://www.instagram.com/LaPlateformeIO/" target="_blank"><img src="view/assets/logo_insta.JPG" alt="logoinstagram" class="logosocial"></a></div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </footer>
     
     </body>
 </html>
--- view/gestion-tarifs.php
+++ view/gestion-tarifs.php
@@ -33,16 +33,16 @@ $option3_rate = $options_rate[2]['rate'];
 
         <main>
 
-            <h1>Gestion des Tarifs</h1>
+            <h1 id="tarifs_title">Gestion des Tarifs</h1>
 
 
             <section class="container">
 
+                <div class="container1">
                 <form action ="gestion-tarifs.php" method="post">
                     <label for="day_site_rate">Emplacement / jour<label>
                     <input type="number" id="rate" name="rate" value="<?php echo $site_rate ?>">
-                    <label for="password">Mot de passe<label>
-                    <?php if (isset($err_password_1)) { echo $err_password_1 ;} ?>
+                    <?php if (isset($err_password_1)) { echo '<div class="err_form">'.$err_password_1.'</div>' ;} ?>
                     <input type="password" name="password" placeholder="Confirmez avec votre mot de passe administrateur">
                     <input type="submit" value="Modifier" name="update_site_rate">
                     
@@ -51,16 +51,18 @@ $option3_rate = $options_rate[2]['rate'];
                 <form action ="gestion-tarifs.php" method="post">
                     <label for="option1_rate">Borne électrique / jour <label>
                     <input type="number" id="option1_rate" name="option1_rate" value="<?php echo $option1_rate ;?>">
-                    <?php if (isset($err_password_2)) { echo $err_password_2 ;} ?>
-                    <input type="password" name="password">
+                    <?php if (isset($err_password_2)) { echo '<div class="err_form"><br>'.$err_password_2.'</div>' ;} ?>
+                    <input type="password" name="password" placeholder="Confirmez avec votre mot de passe administrateur">
                     <input type="submit" value="Modifier" name="update_option1_rate">
                   
                 </form>
-
+                </div>
+                
+                <div class="container2">
                 <form action ="gestion-tarifs.php" method="post">
                     <label for="option2_rate">Accès DiscoClub / jour <label>
                     <input type="number" id="option2_rate" name="option2_rate" value="<?php echo $option2_rate ;?>">
-                    <?php if (isset($err_password_3)) { echo $err_password_3 ;} ?>
+                    <?php if (isset($err_password_3)) { echo '<div class="err_form"><br>'.$err_password_3.'</div>' ;} ?>
                     <input type="password" name="password" placeholder="Confirmez avec votre mot de passe administrateur">
                     <input type="submit" value="Modifier" name="update_option2_rate">
                     
@@ -69,18 +71,18 @@ $option3_rate = $options_rate[2]['rate'];
                 <form action ="gestion-tarifs.php" method="post">
                     <label for="option3_rate">Pack activités / jour<label>
                     <input type="number" id="option3_rate" name="option3_rate" value="<?php echo $option3_rate ;?>">
-                    <?php if (isset($err_password_4)) { echo $err_password_4 ;} ?> 
+                    <?php if (isset($err_password_4)) { echo '<div class="err_form"><br>'.$err_password_4.'</div>' ;} ?> 
                     <input type="password" name="password" placeholder="Confirmez avec votre mot de passe administrateur">
                     <input type="submit" value="Modifier" name="update_option3_rate">
                     
                 </form>
-
-               
+                </div>
 
             </section>
-
-        
         </main>
+
+        <?php require('footer.php'); ?>
+
     </body>
 </html>
 
